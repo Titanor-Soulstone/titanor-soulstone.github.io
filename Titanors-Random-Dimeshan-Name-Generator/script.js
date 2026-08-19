f3a0d2fb73a76622db72cd62b43c87bafef2ba2c
Titanor Soulstone <sir.titas@yahoo.com.au>
2026-08-19 17:21:24 +0800

    Update script.js

--- Titanors-Random-Dimeshan-Name-Generator/script.js
+++ Titanors-Random-Dimeshan-Name-Generator/script.js
@@ -132,7 +132,7 @@ function button_call_generate_names() {
 		names.unshift(name);
 		namehistory.unshift(name);
 		if (namehistory.length > 100) {
-			namehistory.pop();
+			namehistory.splice(-1, 1);
 		}
 	}
 	console.log(names);
